51c156bfc18422b68ad7d1d602886f1d5d84ce2a
DJovellar <davidjovellar93@gmail.com>
2019-02-26 09:29:22 +0100

    Weather-client correcto

--- Weather-client.py
+++ Weather-client.py
@@ -2,6 +2,8 @@
 
 from urllib.request import urlopen
 import bs4
+import json
+import pprint
 
 
 class Clientweb(object):
@@ -12,19 +14,28 @@ class Clientweb(object):
 
     #Modificar url por la de la api de la web openweathermap
     def do_request(self):
-        #https://api.openweathermap.org/data...
-        f = urlopen("http://www.eps.udl.cat/ca/")
-        data = file.read()
+        #https://api.openweathermap.org/data/2.5/find?q=Lleida&units=metric&appid=7de50c11e01dc42f66131cb4c8c0dc10
+        ##https://api.openweathermap.org/data/2.5/find?q=Lleida&units=metric&appid=2a5b07d9b473350f8b06a8f47273d903&mode=json&lang=ca
+        f = urlopen("https://api.openweathermap.org/data/2.5/find?q=Lleida&units=metric&appid=2a5b07d9b473350f8b06a8f47273d903&mode=json&lang=ca")
+        data = f.read()
         f.close()
         return data
 
-    def process_weather(self, html):
-        arbre = bs4.BeautifulSoup(html, features="lxml")
-        temperature = arbre.find("temperature")
-        weather = arbre.find("weather")
-        print(temperature["value"] + "and" + weather["value"])
+    #def process_weather(self, html):
+    #    arbre = bs4.BeautifulSoup(html, features="lxml")
+    #    temperature = arbre.find("temperature")
+    #    weather = arbre.find("weather")
+    #    print(temperature["value"] + "and" + weather["value"])
 
-        return None
+    #    return None
+
+    #Usar esta si es posible!
+    def process_weather(self, html):
+        dic = json.loads(html)
+        pprint.pprint(dic)
+        temp = dic['list'][0]['main']['temp']
+        weath = dic['list'][0]['weather'][0]['description']
+        return (temp + " and " + weath)
 
     def run(self):
         # descargar pagina web en html
